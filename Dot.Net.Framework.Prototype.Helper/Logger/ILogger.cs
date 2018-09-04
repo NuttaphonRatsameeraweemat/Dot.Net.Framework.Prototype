313c4c9234ef90d10aff69b10fea1f9b5f9e3868
nuttaphon <nuttaphon@leaderplanet.co.th>
2018-09-04 15:25:36 +0700

    Add project files.

---
+++ Dot.Net.Framework.Prototype.Helper/Logger/ILogger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Dot.Net.Framework.Prototype.Helper.Logger
+{
+    /// <summary>
+    /// Interface ILogger provides logging to track all activities by using NLog library.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Creates a new session for logging.
+        /// </summary>
+        void CreateNewSession();
+
+        /// <summary>
+        /// Logs the debug, the message will be logged when debug mode is on.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Debug(string message);
+
+        /// <summary>
+        /// Logs the error.
+        /// </summary>
+        /// <param name="ex">The exception.</param>
+        /// <param name="message">The additional message.</param>
+        void Error(Exception ex, string message = null);
+
+        /// <summary>
+        /// Logs the information such as start and end.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        void Info(string message);
+    }
+}
---
+++ Dot.Net.Framework.Prototype/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Dot.Net.Framework.Prototype.Global" Language="C#" %>
