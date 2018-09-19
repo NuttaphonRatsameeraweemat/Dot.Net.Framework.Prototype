ce43de18615ed262990f36aaedca46c177c1e80b
Nuttaphon Ratsameeraweemat <nuttaphon.rat@gmail.com>
2018-09-19 21:42:01 +0700

    Update -Remove Pocos Entity

--- Dot.Net.Framework.Prototype.Data/DSUnitOfWork.cs
+++ Dot.Net.Framework.Prototype.Data/DSUnitOfWork.cs
@@ -1,4 +1,5 @@
 ﻿using Dot.Net.Framework.Prototype.Data.Repository.EF;
+using System.Data.Entity;
 using System.Data.Entity.Infrastructure.Interception;
 using System.Data.Entity.SqlServer;
 
@@ -7,15 +8,15 @@ namespace Dot.Net.Framework.Prototype.Data
     /// <summary>
     /// MdwUnitOfWork class is a unit of work for manipulating about utility data in database via repository.
     /// </summary>
-    public class DSUnitOfWork : UnitOfWork<DSDBContext>
+    public class DSUnitOfWork : UnitOfWork<DbContext>
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DSUnitOfWork" /> class.
         /// </summary>
-        /// <param name="digitalsignatureDbContext">The digital signature database context what inherits from DbContext of EF.</param>
+        /// <param name="dbContext">The database context what inherits from DbContext of EF.</param>
         /// <param name="dbCommandInterceptor">The database command interceptor for logging.</param>
-        public DSUnitOfWork(DSDBContext digitalsignatureDbContext, IDbCommandInterceptor dbCommandInterceptor = null)
-            : base(digitalsignatureDbContext, SqlProviderServices.Instance, dbCommandInterceptor)
+        public DSUnitOfWork(DbContext dbContext, IDbCommandInterceptor dbCommandInterceptor = null)
+            : base(dbContext, SqlProviderServices.Instance, dbCommandInterceptor)
         { }
     }
 }
